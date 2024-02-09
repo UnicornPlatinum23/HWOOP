9506ef05d96e34831fa2316901c4c1c1be506333
Arland Gamble <agdigital2030@gmail.com>
2024-02-09 15:51:28 -0500

    first

---
+++ app.js
@@ -0,0 +1,28 @@
+
+function compareValue(y, z){
+    if (y == z){
+    return "values are equal";
+} else {
+    return "values are different";
+}
+
+}
+
+ console.log(compareValue(20));
+ console.log(compareValue(40));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("app.js");
